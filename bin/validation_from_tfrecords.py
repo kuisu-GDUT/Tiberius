beceb34f15657c280cdd0682e7b7971ba32c8f29
kuisu <sukui@genomics.cn>
2024-11-29 14:54:46 +0800

    update about val model

--- bin/validation_from_tfrecords.py
+++ bin/validation_from_tfrecords.py
@@ -4,7 +4,7 @@
 # Get validation data from tfrecords files. 
 # Creates a numpy array from sampels from tfrecords files.
 # ==============================================================
-
+import glob
 import os, sys, json, argparse
 import numpy as np
 from data_generator import DataGenerator
@@ -26,8 +26,13 @@ def read_species(file_name):
 
 def main():
     args = parseCmd()
-    species = read_species(args.species)
-    file_paths = [f'{args.tfrec_dir}/{s}_{i}.tfrecords' for s in species for i in range(args.tfrec_per_species)]
+    # species = read_species(args.species)
+    # file_paths = [f'{args.tfrec_dir}/{s}_{i}.tfrecords' for s in species for i in range(args.tfrec_per_species)]
+
+    species = read_species(f'{args.tfrec_dir}/{args.species}')
+    file_paths = []
+    for specie in species:
+        file_paths.extend(glob.glob(f'{args.tfrec_dir}/{specie}_*.tfrecords'))
     data_x = []
     data_y = []
     data_clamsa = []
@@ -37,7 +42,7 @@ def main():
             shuffle=True, 
             repeat=True,
             filter=False,
-            output_size=15,
+            output_size=args.output_size,
             hmm_factor=0,
             trans=False,clamsa=args.clamsa
       )   
@@ -52,6 +57,10 @@ def main():
         data_x.append(example_x[i])
         data_y.append(example_y[i])
 
+    data_y = np.array(data_y)
+    data_x = np.array(data_x)
+    print(f"data_x shape: {data_x.shape}; data_y shape: {data_y.shape}")
+    print(f"save in {args.out}")
     if args.clamsa:
         np.savez(args.out, array1=np.array(data_x), array2=np.array(data_y), array3=np.array(data_clamsa))
     else:
@@ -81,7 +90,9 @@ def parseCmd():
         help='Number of trainings examples in the output.')
     parser.add_argument('--clamsa', action='store_true')  
     parser.add_argument('--out', type=str,
-        help='Output name of numpy array.', default='validation_data')        
+        help='Output name of numpy array.', default='validation_data')
+    parser.add_argument('--output_size', type=str,
+                        help='output size, including 7, 15', default=15)
     return parser.parse_args()
 
 if __name__ == '__main__':
